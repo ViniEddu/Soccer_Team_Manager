5665622424e82e477da1592aefc5c04ac414dc2d
Vinicius Eduardo <vinicius.eddu99@gmail.com>
2023-04-29 05:04:29 -0300

    nada de mais

--- src/app.js
+++ src/app.js
@@ -15,7 +15,29 @@ const teams = [
     },
   ];
 
-app.get('/teams', (req, res) => res.status(200).json({ teams }));
+const validateID = (req, res, next) => {
+  const { id } = req.params;
+  const idAsNumber = Number(id);
+
+  if (Number.isNaN(idAsNumber)) {
+    res.status(400).send({ message: 'Formato de ID inválido! Precisa ser um número' });
+  } else {
+    next();
+  }
+};
+
+const validateTeamsData = (req, res, next) => {
+  const requiredProperties = ['name', 'initials'];
+
+  if (requiredProperties.every((property) => property in req.body)) {
+    next();
+  } else {
+    res.status(400).send({ message: 'O time precisa receber os atributos name e initials' });
+  }
+};
+
+app.get('/teams', (_req, res) => res.status(200).json({ teams }));
+
 app.post('/teams', (req, res) => {
     const newTeam = { ...req.body };
     teams.push(newTeam);
@@ -23,7 +45,7 @@ app.post('/teams', (req, res) => {
     res.status(201).json({ team: newTeam });
   });
 
-app.put('/teams/:id', (req, res) => {
+app.put('/teams/:id', validateID, validateTeamsData, (req, res) => {
   const { id } = req.params;
   const { name, initials } = req.body;
 
@@ -37,7 +59,7 @@ app.put('/teams/:id', (req, res) => {
   res.status(200).json({ updateTeam });
 });
 
-app.delete('/teams/:id', (req, res) => {
+app.delete('/teams/:id', validateID, (req, res) => {
   const { id } = req.params;
   const arrayPosition = teams.findIndex((team) => team.id === Number(id));
   teams.splice(arrayPosition, 1);
